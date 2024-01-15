a42a70769ec59b62258789e148d4eab9c1b21e27
dariogemo <147483197+dariogemo@users.noreply.github.com>
2024-01-15 11:59:53 +0100

    deleted lead variable, major graphs improvement in water_streamlit.py (boxplots, histograms and violinplot now display before and after data cleaning, instead of only after)

--- water_streamlit.py
+++ water_streamlit.py
@@ -1,13 +1,16 @@
 import pandas as pd
 import polars as pl
 import streamlit as st 
+import seaborn as sns
+import matplotlib.pyplot as plt 
 
 st.title('Water quality detection project')
 
 box_sections = st.selectbox('Choose what you want to see of the project', ['Description', 'Exploratory Data Analysis', 'Plots', 'Model'])
 
 if box_sections == 'Description':
-       st.write('prova')
+       st.write('The full code for this project can be found at ')
+       st.link_button('Github', 'https://github.com/dariogemo/water_project')
 
 if box_sections == 'Exploratory Data Analysis':
        import io
@@ -21,6 +24,7 @@ if box_sections == 'Exploratory Data Analysis':
                             'Water Temperature', 'Air Temperature', 'Month', 'Day', 'Time of Day',
                             'Target']
               st.write(df.head(5))
+              st.write(df.tail(5))
               '''
               General informations for our water quality dataset:
               '''
@@ -33,10 +37,11 @@ if box_sections == 'Exploratory Data Analysis':
        if st.checkbox('After Cleaning'):
               df = pl.read_csv('csv\Water_Quality_Prediction_Clean.csv')
               df = pd.DataFrame(df)
-              df.columns = ['pH', 'Iron', 'Nitrate', 'Chloride', 'Lead', 'Zinc', 'Color',
+              df.columns = ['pH', 'Iron', 'Nitrate', 'Chloride', 'Zinc', 'Color',
                             'Turbidity', 'Fluoride', 'Copper', 'Odor', 'Sulfate', 'Conductivity',
                             'Chlorine', 'Manganese', 'Total_Diss_Solids', 'Potability']
               st.write(df.head(5))
+              st.write(df.tail(5))
               '''
               General informations for our water quality dataset:
               '''
@@ -61,120 +66,120 @@ if box_sections == 'Plots':
        '''
        st.image('images\heatmap.png')
        '''
-       It's important that our cleaning of the dataset didn't impact too much the distributions of our variables. 
-       If that was the case, we might encounter some problems in the model part of the project.
+       It's important that our cleaning of the dataset didn't impact too much our variables. 
+       If that was the case, we might encounter some lower performances in the model part of the project.
        '''
-       box_distr_features = st.selectbox('Distribution of the variable:', ['None', 'pH', 'Iron', 'Nitrate', 
-                                                                           'Chloride', 'Lead', 'Zinc', 'Color', 
-                                                                           'Turbidity', 'Fluoride', 'Copper', 'Odor', 
-                                                                           'Sulfate', 'Conductivity', 'Chlorine', 
-                                                                           'Manganese', 'Total_Diss_Solids'], 
-                                                                            label_visibility = 'collapsed')
-       if box_distr_features == 'None':
-              st.image('images\distr\iNone.png')
-       if box_distr_features == 'pH':
-              st.image('images\distr\pH.png')
-       if box_distr_features == 'Iron':
-              st.image('images\distr\Iron.png')
-       if box_distr_features == 'Nitrate':
-              st.image('images\distr\iNitrate.png')
-       if box_distr_features == 'Chloride':
-              st.image('images\distr\Chloride.png')
-       if box_distr_features == 'Zinc':
-              st.image('images\distr\Zinc.png')
-       if box_distr_features == 'Color':
-              st.image('images\distr\Color.png')
-       if box_distr_features == 'Turbidity':
-              st.image('images\distr\Turbidity.png')
-       if box_distr_features == 'Fluoride':
-              st.image('images\distr\Fluoride.png')
-       if box_distr_features == 'Copper':
-              st.image('images\distr\Copper.png')
-       if box_distr_features == 'Odor':
-              st.image('images\distr\Odor.png')
-       if box_distr_features == 'Sulfate':
-              st.image('images\distr\Sulfate.png')
-       if box_distr_features == 'Conductivity':
-              st.image('images\distr\Conductivity.png')
-       if box_distr_features == 'Chlorine':
-              st.image('images\distr\Chlorine.png')
-       if box_distr_features == 'Manganese':
-              st.image('images\distr\Manganese.png')
-       if box_distr_features == 'Total_Diss_Solids':
-              st.image('images\distr\Total_Diss_Solids.png')
-       if box_distr_features == 'Lead':
-              st.image('images\distr\Lead.png')
-       box_boxplot_features = st.selectbox('Feature', ['None', 'pH', 'Iron', 'Nitrate', 'Chloride', 'Lead', 'Zinc', 
-                                                       'Color','Turbidity', 'Fluoride', 'Copper', 'Odor', 'Sulfate', 
-                                                       'Conductivity', 'Chlorine', 'Manganese', 'Total_Diss_Solids'], label_visibility = 'collapsed')
-       if box_boxplot_features == 'None':
-              st.image('images\iboxplot\iNone.png')
-       if box_boxplot_features == 'pH':
-              st.image('images\iboxplot\pH.png')
-       if box_boxplot_features == 'Iron':
-              st.image('images\iboxplot\Iron.png')
-       if box_boxplot_features == 'Nitrate':
-              st.image('images\iboxplot\iNitrate.png')
-       if box_boxplot_features == 'Chloride':
-              st.image('images\iboxplot\Chloride.png')
-       if box_boxplot_features == 'Lead':
-              st.image('images\iboxplot\Lead.png')
-       if box_boxplot_features == 'Zinc':
-              st.image('images\iboxplot\Zinc.png')
-       if box_boxplot_features == 'Color':
-              st.image('images\iboxplot\Color.png')
-       if box_boxplot_features == 'Turbidity':
-              st.image('images\iboxplot\Turbidity.png')
-       if box_boxplot_features == 'Fluoride':
-              st.image('images\iboxplot\Fluoride.png')
-       if box_boxplot_features == 'Copper':
-              st.image('images\iboxplot\Copper.png')
-       if box_boxplot_features == 'Odor':
-              st.image('images\iboxplot\Odor.png')
-       if box_boxplot_features == 'Sulfate':
-              st.image('images\iboxplot\Sulfate.png')
-       if box_boxplot_features == 'Conductivity':
-              st.image('images\iboxplot\Conductivity.png')
-       if box_boxplot_features == 'Chlorine':
-              st.image('images\iboxplot\Chlorine.png')
-       if box_boxplot_features == 'Manganese':
-              st.image('images\iboxplot\Manganese.png')
-       if box_boxplot_features == 'Total_Diss_Solids':
-              st.image('images\iboxplot\Total_Diss_Solids.png')
-       box_violinplot_features = st.selectbox('Feature violinplot', ['None', 'pH', 'Iron', 'Nitrate', 'Chloride', 'Lead', 'Zinc', 
-                                                       'Color','Turbidity', 'Fluoride', 'Copper', 'Odor', 'Sulfate', 
-                                                       'Conductivity', 'Chlorine', 'Manganese', 'Total_Diss_Solids'], label_visibility = 'collapsed')
-       if box_violinplot_features == 'None':
-              st.image('images\iviolinplot\iNone.png')
-       if box_violinplot_features == 'pH':
-              st.image('images\iviolinplot\pH.png')
-       if box_violinplot_features == 'Iron':
-              st.image('images\iviolinplot\Iron.png')
-       if box_violinplot_features == 'Nitrate':
-              st.image('images\iviolinplot\iNitrate.png')
-       if box_violinplot_features == 'Chloride':
-              st.image('images\iviolinplot\Chloride.png')
-       if box_violinplot_features == 'Lead':
-              st.image('images\iviolinplot\Lead.png')
-       if box_violinplot_features == 'Zinc':
-              st.image('images\iviolinplot\Zinc.png')
-       if box_violinplot_features == 'Color':
-              st.image('images\iviolinplot\Color.png')
-       if box_violinplot_features == 'Turbidity':
-              st.image('images\iviolinplot\Turbidity.png')
-       if box_violinplot_features == 'Fluoride':
-              st.image('images\iviolinplot\Fluoride.png')
-       if box_violinplot_features == 'Copper':
-              st.image('images\iviolinplot\Copper.png')
-       if box_violinplot_features == 'Odor':
-              st.image('images\iviolinplot\Odor.png')
-       if box_violinplot_features == 'Sulfate':
-              st.image('images\iviolinplot\Sulfate.png')
-       if box_violinplot_features == 'Conductivity':
-              st.image('images\iviolinplot\Conductivity.png')
-       if box_violinplot_features == 'Chlorine':
-              st.image('images\iviolinplot\Chlorine.png')
-       if box_violinplot_features == 'Manganese':
-              st.image('images\iviolinplot\Manganese.png')
-       if box_violinplot_features == 'Total_Diss_Solids':
-              st.image('images\iviolinplot\Total_Diss_Solids.png')    
+       box_type_graph = st.selectbox('Choose what type of graph you would like to see:', ['None', 'Histogram', 'Boxplot', 'Violinplot'], key = 'graphs')
+       if box_type_graph == 'None':
+              pass
+       if box_type_graph == 'Histogram': 
+              box_distr_features = st.selectbox('Distribution of the variable:', ['None', 'pH', 'Iron', 'Nitrate', 
+                                                                             'Chloride', 'Zinc', 'Color', 
+                                                                             'Turbidity', 'Fluoride', 'Copper', 'Odor', 
+                                                                             'Sulfate', 'Conductivity', 'Chlorine', 
+                                                                             'Manganese', 'Total_Diss_Solids'], 
+                                                                             label_visibility = 'collapsed', key = 'distr')
+              if box_distr_features == 'None':
+                     st.image('images\distr\iNone.png') 
+              if box_distr_features == 'pH':
+                     st.image('images\distr\pH.png')
+              if box_distr_features == 'Iron':
+                     st.image('images\distr\Iron.png')
+              if box_distr_features == 'Nitrate':
+                     st.image('images\distr\iNitrate.png')
+              if box_distr_features == 'Chloride':
+                     st.image('images\distr\Chloride.png')
+              if box_distr_features == 'Zinc':
+                     st.image('images\distr\Zinc.png')
+              if box_distr_features == 'Color':
+                     st.image('images\distr\Color.png')
+              if box_distr_features == 'Turbidity':
+                     st.image('images\distr\Turbidity.png')
+              if box_distr_features == 'Fluoride':
+                     st.image('images\distr\Fluoride.png')
+              if box_distr_features == 'Copper':
+                     st.image('images\distr\Copper.png')
+              if box_distr_features == 'Odor':
+                     st.image('images\distr\Odor.png')
+              if box_distr_features == 'Sulfate':
+                     st.image('images\distr\Sulfate.png')
+              if box_distr_features == 'Conductivity':
+                     st.image('images\distr\Conductivity.png')
+              if box_distr_features == 'Chlorine':
+                     st.image('images\distr\Chlorine.png')
+              if box_distr_features == 'Manganese':
+                     st.image('images\distr\Manganese.png')
+              if box_distr_features == 'Total_Diss_Solids':
+                     st.image('images\distr\Total_Diss_Solids.png')
+       if box_type_graph == 'Boxplot':
+              box_boxplot_features = st.selectbox('Feature', ['None', 'pH', 'Iron', 'Nitrate', 'Chloride', 'Zinc', 
+                                                        'Color','Turbidity', 'Fluoride', 'Copper', 'Odor', 'Sulfate', 
+                                                        'Conductivity', 'Chlorine', 'Manganese', 'Total_Diss_Solids'], label_visibility = 'collapsed', key = 'boxplot')
+              if box_boxplot_features == 'None':
+                     st.image('images\iboxplot\iNone.png')
+              if box_boxplot_features == 'pH':
+                     st.image('images\iboxplot\pH.png')
+              if box_boxplot_features == 'Iron':
+                     st.image('images\iboxplot\Iron.png')
+              if box_boxplot_features == 'Nitrate':
+                     st.image('images\iboxplot\iNitrate.png')
+              if box_boxplot_features == 'Chloride':
+                     st.image('images\iboxplot\Chloride.png')
+              if box_boxplot_features == 'Zinc':
+                     st.image('images\iboxplot\Zinc.png')
+              if box_boxplot_features == 'Color':
+                     st.image('images\iboxplot\Color.png')
+              if box_boxplot_features == 'Turbidity':
+                     st.image('images\iboxplot\Turbidity.png')
+              if box_boxplot_features == 'Fluoride':
+                     st.image('images\iboxplot\Fluoride.png')
+              if box_boxplot_features == 'Copper':
+                     st.image('images\iboxplot\Copper.png')
+              if box_boxplot_features == 'Odor':
+                     st.image('images\iboxplot\Odor.png')
+              if box_boxplot_features == 'Sulfate':
+                     st.image('images\iboxplot\Sulfate.png')
+              if box_boxplot_features == 'Conductivity':
+                     st.image('images\iboxplot\Conductivity.png')
+              if box_boxplot_features == 'Chlorine':
+                     st.image('images\iboxplot\Chlorine.png')
+              if box_boxplot_features == 'Manganese':
+                     st.image('images\iboxplot\Manganese.png')
+              if box_boxplot_features == 'Total_Diss_Solids':
+                     st.image('images\iboxplot\Total_Diss_Solids.png')
+       if box_type_graph == 'Violinplot':
+              box_violinplot_features = st.selectbox('Feature violinplot', ['None', 'pH', 'Iron', 'Nitrate', 'Chloride', 'Zinc', 
+                                                        'Color','Turbidity', 'Fluoride', 'Copper', 'Odor', 'Sulfate', 
+                                                        'Conductivity', 'Chlorine', 'Manganese', 'Total_Diss_Solids'], label_visibility = 'collapsed', key = 'violinplot')
+              if box_violinplot_features == 'None':
+                     st.image('images\iviolinplot\iNone.png')
+              if box_violinplot_features == 'pH':
+                     st.image('images\iviolinplot\pH.png')
+              if box_violinplot_features == 'Iron':
+                     st.image('images\iviolinplot\Iron.png')
+              if box_violinplot_features == 'Nitrate':
+                     st.image('images\iviolinplot\iNitrate.png')
+              if box_violinplot_features == 'Chloride':
+                     st.image('images\iviolinplot\Chloride.png')
+              if box_violinplot_features == 'Zinc':
+                     st.image('images\iviolinplot\Zinc.png')
+              if box_violinplot_features == 'Color':
+                     st.image('images\iviolinplot\Color.png')
+              if box_violinplot_features == 'Turbidity':
+                     st.image('images\iviolinplot\Turbidity.png')
+              if box_violinplot_features == 'Fluoride':
+                     st.image('images\iviolinplot\Fluoride.png')
+              if box_violinplot_features == 'Copper':
+                     st.image('images\iviolinplot\Copper.png')
+              if box_violinplot_features == 'Odor':
+                     st.image('images\iviolinplot\Odor.png')
+              if box_violinplot_features == 'Sulfate':
+                     st.image('images\iviolinplot\Sulfate.png')
+              if box_violinplot_features == 'Conductivity':
+                     st.image('images\iviolinplot\Conductivity.png')
+              if box_violinplot_features == 'Chlorine':
+                     st.image('images\iviolinplot\Chlorine.png')
+              if box_violinplot_features == 'Manganese':
+                     st.image('images\iviolinplot\Manganese.png')
+              if box_violinplot_features == 'Total_Diss_Solids':
+                     st.image('images\iviolinplot\Total_Diss_Solids.png')    
